ca1561a766ae02bcaf9baf6c40e8a7c63fae3578
kutsikas27 <mardus80@gmail.com>
2023-04-12 16:56:43 +0300

    logo update

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import ToggleMode from "./ToggleMode";
-import reactLogo from "../icons/logo4.png";
+import reactLogo from "../icons/logo11.png";
 
 const Header = () => {
   return (
