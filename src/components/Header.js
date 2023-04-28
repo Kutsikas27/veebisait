fc3f301002c21647a18ae03662248c67caf960bd
kutsikas27 <mardus80@gmail.com>
2023-04-28 17:15:03 +0300

    logod

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import React from "react";
 import styled from "styled-components";
 
 import ToggleMode from "./ToggleMode";
-import reactLogo from "../icons/logo11.png";
+import reactLogo from "../icons/pikkLogo.png";
 
 const Header = () => {
   return (
